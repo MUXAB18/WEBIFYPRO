63ea9c39586559324fbf06db2d354296f4a0bbb9
musab-18 <musabiftikhar12@icloud.com>
2026-03-10 17:23:17 +0500

    Fix nodemailer IPv6 connection error on Render

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
